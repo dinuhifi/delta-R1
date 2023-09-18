9fdb08ea6996690e4728fe1cbef3217ed078c2a4
dinuhifi <dinesh23110077@snuchennai.edu.in>
2023-09-18 13:45:43 +0530

    added gitignore and started working in frontend

---
+++ frontend/E_commerce_cart.js
@@ -0,0 +1,10 @@
+main()
+
+function main() {
+    var data = [3, 7, 2, 9, 1, 11];
+    var sum = 0;
+    for (let i = 0; i < data.length; i++) {
+        sum += data[i]
+    }
+    console.log(sum)
+}
